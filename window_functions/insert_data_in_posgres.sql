a8729556810ccf3e876f63495828a53266f83cdf
Prabhakara Pelluru <prabhakarapelluru@Prabhakaras-Mac-Book-Pro-2022.local>
2024-11-25 21:15:01 -0800

    added folder for window functions

---
+++ window_functions/insert_data_in_posgres.sql
@@ -0,0 +1,38 @@
+i-- postgress sql window functions example
+
+
+DROP TABLE IF EXISTS products;
+CREATE TABLE products (
+	product_id serial PRIMARY KEY,
+	product_name VARCHAR (255) NOT NULL,
+	price DECIMAL (11, 2),
+	group_id INT NOT NULL,
+	FOREIGN KEY (group_id) REFERENCES product_groups (group_id)
+);
+
+DROP TABLE IF EXISTS product_groups CASCADE;
+CREATE TABLE product_groups (
+group_id serial PRIMARY KEY,
+	group_name VARCHAR (255) NOT NULL
+);
+
+
+INSERT INTO product_groups (group_name)
+VALUES
+	('Smartphone'),
+	('Laptop'),
+	('Tablet');
+INSERT INTO products (product_name, group_id,price)
+VALUES
+	('Microsoft Lumia', 1, 200),
+	('HTC One', 1, 400),
+	('Nexus', 1, 500),
+	('iPhone', 1, 900),
+	('HP Elite', 2, 1200),
+	('Lenovo Thinkpad', 2, 700),
+	('Sony VAIO', 2, 700),
+	('Dell Vostro', 2, 800),
+	('iPad', 3, 700),
+	('Kindle Fire', 3, 150),
+	('Samsung Galaxy Tab', 3, 200);
+
